5eab9237dfd4bf09733ee2026e15f05f746a9d06
uib18572 <steven.pleyer@continental-corporation.com>
2020-11-11 10:42:43 +0100

    Added needed UI elements; Added button events for all answers; Added new @strings for error finding later; //TODO: No questions and answers are yet implemented into the UI;

--- app/src/main/java/com/example/solvemedis/Math_Normal.java
+++ app/src/main/java/com/example/solvemedis/Math_Normal.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Math_Normal extends AppCompatActivity {
     private Button back_button;
+    private Button answer_button_1;
+    private Button answer_button_2;
+    private Button answer_button_3;
+    private Button answer_button_4;
+    private TextView questionBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,46 @@ public class Math_Normal extends AppCompatActivity {
 
             }
         });
+        answer_button_1 = (Button) findViewById(R.id.normal_answer_button_1);
+        answer_button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer();
+
+            }
+        });
+        answer_button_2 = (Button) findViewById(R.id.normal_answer_button_2);
+        answer_button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer();
+
+            }
+        });
+        answer_button_3 = (Button) findViewById(R.id.normal_answer_button_3);
+        answer_button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer();
+
+            }
+        });
+        answer_button_4 = (Button) findViewById(R.id.normal_answer_button_4);
+        answer_button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer();
+
+            }
+        });
     }
 
-    public void openHome() {
+    public void openHome(){
         Intent intent_home = new Intent(this, MainActivity.class);
         startActivity(intent_home);
+    }
+
+    public void checkAnswer(){
+
     }
 }
